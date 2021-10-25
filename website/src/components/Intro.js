2f5c6c01d957ac6d339ba94f4eb31372f1c9dc9a
cveigt <cveigt@gmail.com>
2021-10-25 00:01:09 -0700

    Added Intro component structure and basic styling

--- website/src/components/Intro.js
+++ website/src/components/Intro.js
@@ -1,10 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  background: #f1eff4;
+  height: 200px;
+`;
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+  margin: auto;
+  margin-top: 18vh;
+  margin-bottom: 24vh;
+`;
+const Presentation = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.h2`
+  font-size: 2.6rem;
+  line-height: 3.1rem;
+  margin-bottom: 1.5rem;
+`;
+const Description = styled.p`
+  font-size: 1.3rem;
+  line-height: 1.9rem;
+`;
 
 const Intro = () => {
   return (
-    <div>
-      <h1>Intro section</h1>
-    </div>
+    <Section>
+      <Content>
+        <Presentation>
+          <Title>Hey there, I’m Christian Veigt</Title>
+          <Description>
+            A product designer and developer in Vancouver, currently working on
+            design systems <a href="\">@hashicorp</a>.
+            <br />
+            Previously, I designed <a href="\">@twilio</a>.
+          </Description>
+        </Presentation>
+      </Content>
+    </Section>
   );
 };
 
