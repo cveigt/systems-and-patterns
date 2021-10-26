569a5e1956f43a7124b26b27b3cd29ee18f1b3b8
cveigt <cveigt@gmail.com>
2021-10-25 18:22:38 -0700

    Adding basic structure to Navbar component

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -1,10 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const NavbarContainer = styled.div``;
+const NavbarWrap = styled.div``;
+const Logo = styled.div``;
+const Nav = styled.nav``;
+const NavLink = styled(Link)``;
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navigation</h1>
-    </div>
+    <NavbarContainer>
+      <NavbarWrap>
+        <Logo></Logo>
+        <Nav>
+          <NavLink to="/">Work</NavLink>
+          <NavLink to="/">Blog</NavLink>
+          <NavLink to="/">Contact</NavLink>
+        </Nav>
+      </NavbarWrap>
+    </NavbarContainer>
   );
 };
 
