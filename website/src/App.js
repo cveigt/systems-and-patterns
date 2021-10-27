2f412b6b57b3a9098a63502fb1b2501b40daaeb3
cveigt <cveigt@gmail.com>
2021-10-26 21:56:40 -0700

    Installed Flight icons library

--- website/src/App.js
+++ website/src/App.js
@@ -4,12 +4,16 @@ import Card from "./components/Card";
 import GlobalStyles from "./components/styles/Global";
 import CardImg from "./assets/card-image.png";
 
+// const flightArrowRight =
+//   require("@hashicorp/flight-icons/svg/arrow-right-16.svg").default;
+
 function App() {
   return (
     <>
       <GlobalStyles />
       <div>
         <Navbar />
+        {/* <img src={flightArrowRight} /> */}
         <Intro />
         <Card img={CardImg} alt="Components of a Design System" />
       </div>
