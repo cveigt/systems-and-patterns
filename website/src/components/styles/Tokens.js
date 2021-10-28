b5a5a84e5e525ff8064a4e837ec41acbb59fc764
cveigt <cveigt@gmail.com>
2021-10-27 22:00:01 -0700

    Added first CSS variables as design tokens

---
+++ website/src/components/styles/Tokens.js
@@ -0,0 +1,9 @@
+// Design tokens
+
+const COLORS = {
+  primary: "#100427",
+  secondary: "#601BF2",
+  white: "#ffffff",
+};
+
+export default COLORS;
---
+++ website/src/components/styles/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  color: {
+    primary: "#100427",
+    secondary: "#601BF2",
+    white: "#FFFFFF",
+  },
+
+  space: ["4px", "8px", "12px", "16px", "20px", "24px", "32px"],
+
+  font: {
+      heading: 'font-family: 'Inter', sans-serif',
+      body: 'font-family: 'Inter', sans-serif',
+  },
+
+  fontSize: {
+      sm: '14px',
+      md: '16px',
+      lg: '32px',
+  }
+};
